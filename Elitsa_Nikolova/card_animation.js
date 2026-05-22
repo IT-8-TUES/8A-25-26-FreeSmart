6685ec29c7fba2c715e481592b99738914cfb2b3
enikolova2000 <e.nikolova2000@gmail.com>
2026-05-22 23:36:05 +0300

    fadeinifix

--- Elitsa_Nikolova/card_animation.js
+++ Elitsa_Nikolova/card_animation.js
@@ -1,13 +1,15 @@
 function revealOnScroll() {
   const reveals = document.querySelectorAll('.reveal');
-  
+
   reveals.forEach(element => {
     const windowHeight = window.innerHeight;
     const elementTop = element.getBoundingClientRect().top;
-    const elementVisible = 85; // How many pixels of the element must be seen before animating
-    
-    if (elementTop < windowHeight - elementVisible) {
+    const elementVisible = 30;
+
+    if (elementTop < windowHeight - elementVisible && elementTop > -element.offsetHeight) {
       element.classList.add('active');
+    } else {
+      element.classList.remove('active');
     }
   });
 }
